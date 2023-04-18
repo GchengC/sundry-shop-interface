69d149d6ddf05c2e33b5a3764f67a9baab68dc5b
Guillermo Cheng <34964530+GchengC@users.noreply.github.com>
2023-04-17 22:46:23 -0400

    Corte 17/04/2023

---
+++ SundryShopInterface/src/test/java/com/portal/sundryshop/web/SundryShopInterfaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portal.sundryshop.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SundryShopInterfaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
